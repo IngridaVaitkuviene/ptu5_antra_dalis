222dce983b6d90ca2dcc25a241f70dc33a6502b1
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-10-05 16:11:09 +0300

    new topic Iterators, Generators, in class

---
+++ _1005_generators.py
@@ -0,0 +1,8 @@
+def square_numbers(nums):
+    result = []
+    for i in nums:
+        result.append (i*i)
+    return result
+
+my_nums = square_numbers([1, 2, 3, 4, 5])
+print(my_nums) # [1, 4, 9, 16, 25]
