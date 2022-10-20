bf81becb5b872654393d6a48edf7007144c01196
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-10-20 12:16:20 +0300

    added treeview

--- _1014_darbuotojai_3tkinter.py
+++ _1014_darbuotojai_3tkinter.py
@@ -10,13 +10,45 @@
 from datetime import date
 from tkinter import *
 from _1014_darbuotojai_2valdymas import *
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 # langas
 langas = Tk()
-langas.geometry("1000x600")
-langas.title("Darbuotojų sąrašo administravimas v1.0")
+langas.geometry("1500x800")
+langas.title("Darbuotojų sąrašo Nr. 4 administravimas v1.0")
 ikonele = PhotoImage(file="image/package.png")
 langas.iconphoto(True, ikonele)
+
+columns = ('id', 'name', 'surname', 'birthday', 'position', 'salary', 'year')
+
+tree = ttk.Treeview(langas, columns=columns, show='headings')
+
+tree.heading('id', text='id')
+tree.heading('name', text='Vardas')
+tree.heading('surname', text='Pavarde')
+tree.heading('birthday', text='Gimimo metai')
+tree.heading('position', text='Pareigos')
+tree.heading('salary', text='Atlyginimas')
+tree.heading('year', text='Dirba nuo')
+
+darbuotoju_sarasas = []
+darbuotojai = session.query(Darbuotojai).all()
+for darbuotojas in darbuotojai:
+    darbuotoju_sarasas.append((darbuotojas.id, 
+    darbuotojas.name, darbuotojas.surname, 
+    darbuotojas.birthday, darbuotojas.position,
+    darbuotojas.salary, darbuotojas.year)
+)
+
+for darbuotojai in darbuotoju_sarasas:
+    tree.insert('', END, values=darbuotojai)
+
+def item_selected(event):
+    for selected_item in tree.selection():
+        item = tree.item(selected_item)
+        record = str(item['values'])
+        showinfo(title='Information', message=' '.join(record))
 
 def darbuotojo_ivedimas():
     ivesta_vardas = laukas_vardas.get()
@@ -43,7 +75,7 @@ def isvalyti_sarasa():
     boxas.delete(0, len(employees))
 
 def darbuotojo_redagavimas():
-    darbuotojo_id = laukas_trinimui.get()
+    darbuotojo_id = laukas_trynimui.get()
     darbuotojas = session.query(Darbuotojai).get(darbuotojo_id)
     vardas = laukas_redag_vardas.get()
     pavarde = laukas_redag_pavarde.get()
@@ -51,17 +83,17 @@ def darbuotojo_redagavimas():
     pareigos = laukas_redag_pareigos.get()
     atlyginimas = laukas_redag_atlyginimas.get()
     if len(vardas) > 0:
-        darbuotojas.vardas = vardas
-    if len(pavarde) > 0:
-        darbuotojas.pavarde = pavarde
-    if gimtadienis:
-        darbuotojas.gimtadienis = datetime.datetime.strptime(gimtadienis, "%Y-%m-%d")
-    if len(pareigos) > 0:
-        darbuotojas.pareigos = pareigos
-    if atlyginimas:
-        darbuotojas.atlyginimas = atlyginimas
+        darbuotojas.name = vardas
+    elif len(pavarde) > 0:
+        darbuotojas.surname = pavarde
+    elif gimtadienis:
+        darbuotojas.birthday = datetime.datetime.strptime(gimtadienis, "%Y-%m-%d")
+    elif len(pareigos) > 0:
+        darbuotojas.position = pareigos
+    elif atlyginimas:
+        darbuotojas.salary = atlyginimas
     session.commit()
-    laukas_trinimui.delete(0, len(darbuotojo_id))
+    laukas_trynimui.delete(0, len(darbuotojo_id))
     laukas_redag_vardas.delete(0, len(vardas))
     laukas_redag_pavarde.delete(0, len(pavarde))
     laukas_redag_gimtadienis.delete(0, len(gimtadienis))
@@ -69,11 +101,11 @@ def darbuotojo_redagavimas():
     laukas_redag_atlyginimas.delete(0, len(atlyginimas))
 
 def darbuotojo_istrinimas():
-    darbuotojo_id = laukas_trinimui.get()
+    darbuotojo_id = laukas_trynimui.get()
     darbuotojas = session.query(Darbuotojai).get(darbuotojo_id)
     session.delete(darbuotojas)
     session.commit()
-    laukas_trinimui.delete(0, len(darbuotojo_id))
+    laukas_trynimui.delete(0, len(darbuotojo_id))
 
 # uzrasai
 uzrasas_ivesti = Label(langas, text="Įveskite darbuotoją:")
@@ -96,7 +128,7 @@ laukas_pavarde = Entry(langas)
 laukas_gimtadienis = Entry(langas)
 laukas_pareigos = Entry(langas)
 laukas_atlyginimas = Entry(langas)
-laukas_trinimui = Entry(langas)
+laukas_trynimui = Entry(langas)
 laukas_redag_vardas = Entry(langas)
 laukas_redag_pavarde = Entry(langas)
 laukas_redag_gimtadienis = Entry(langas)
@@ -138,7 +170,7 @@ laukas_pavarde.grid(row=2, column=1)
 laukas_gimtadienis.grid(row=3, column=1)
 laukas_pareigos.grid(row=4, column=1)
 laukas_atlyginimas.grid(row=5, column=1)
-laukas_trinimui.grid(row=12, column=1)
+laukas_trynimui.grid(row=12, column=1)
 laukas_redag_vardas.grid(row=13, column=1)
 laukas_redag_pavarde.grid(row=14, column=1)
 laukas_redag_gimtadienis.grid(row=15, column=1)
@@ -151,7 +183,14 @@ mygtukas_isvalyti.grid(row=10, column=4, ipadx=30, sticky=W)
 mygtukas_trinimui.grid(row=12, column=3, ipadx=30, sticky=W)
 mygtukas_redagavimo.grid(row=18, column=1, ipadx=30, sticky=W)
 
-boxas.grid(row=1, rowspan=9, column=3, columnspan=10, sticky=W)
+boxas.grid(row=1, rowspan=9, column=3, columnspan=13, sticky=W)
 boxas_scroll.grid(row=1, rowspan=9, column=13, ipady=50)
+
+tree.bind('<<TreeviewSelect>>', item_selected)
+tree.grid(row=19, column=0, columnspan=13, sticky='nsew')
+
+scrollbar = ttk.Scrollbar(langas, orient=VERTICAL, command=tree.yview)
+tree.configure(yscroll=scrollbar.set)
+scrollbar.grid(row=19, column=13, sticky='ns')
 
 langas.mainloop()
